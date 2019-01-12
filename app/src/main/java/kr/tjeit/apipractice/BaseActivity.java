39f22584ac80a72a64e77b023061e1a1c0bdf51a
Hottory <gkffndnl123@naver.com>
2019-01-12 15:34:20 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/apipractice/BaseActivity.java
@@ -0,0 +1,17 @@
+package kr.tjeit.apipractice;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext ;
+
+
+    public abstract void setupEvents();
+    public abstract  void  setValues();
+    public abstract void bindView();
+
+
+
+}
---
+++ app/src/main/java/kr/tjeit/apipractice/LoginActivity.java
@@ -0,0 +1,32 @@
+package kr.tjeit.apipractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindView();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindView() {
+
+    }
+}

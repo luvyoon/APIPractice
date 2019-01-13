3793d2f1539701e5adfa87a3c809de1625d9b19a
Hottory <gkffndnl123@naver.com>
2019-01-13 10:41:45 +0900

    메인액티비티 초안 구성

--- app/src/main/java/kr/tjeit/apipractice/MainActivity.java
+++ app/src/main/java/kr/tjeit/apipractice/MainActivity.java
@@ -3,11 +3,31 @@ package kr.tjeit.apipractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        bindView();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindView() {
+
     }
 }
